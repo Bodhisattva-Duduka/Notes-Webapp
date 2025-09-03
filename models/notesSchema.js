07937e56e38686143a98385ffc3d9a48c39b4e76
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-09-04 01:55:53 +0530

    changed notesSchema 'id' to String type

--- models/notesSchema.js
+++ models/notesSchema.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const notesSchema = new mongoose.Schema({
+    id : String,
     title : { type : String , required : true },
     noteBody : String,
-    id : Number
 })
 
 module.exports = mongoose.model('notesSchema' , notesSchema)
